e20396c9028c4cbdb022088c4ae2621fba1d65c2
Yuriy Egorov <yuriy.egorov@team.bumble.com>
2025-01-24 16:01:38 +0000

    Sample project

---
+++ ProTuner/ProTuner/ProTunerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProTunerApp.swift
+//  ProTuner
+//
+//  Created by Yuriy Egorov on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ProTunerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProTuner/ProTunerTests/ProTunerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ProTunerTests.swift
+//  ProTunerTests
+//
+//  Created by Yuriy Egorov on 1/24/25.
+//
+
+import Testing
+
+struct ProTunerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
